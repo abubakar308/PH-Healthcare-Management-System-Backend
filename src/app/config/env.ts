b1bb8eec96b6603b0240c8fe126d8952345647cf
abubakar308 <mdabubakarsiddique789@gmail.com>
2026-03-09 23:45:58 +0600

    file upload functionality implemented

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -16,7 +16,7 @@ interface EnvConfig {
     REFRESH_TOKEN_EXPIRES_IN: string;
     BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string;
     BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string;
-     EMAIL_SENDER:{
+    EMAIL_SENDER: {
         SMTP_USER: string;
         SMTP_PASS: string;
         SMTP_HOST: string;
@@ -27,6 +27,11 @@ interface EnvConfig {
     GOOGLE_CLIENT_SECRET: string;
     GOOGLE_CALLBACK_URL: string;
     FRONTEND_URL: string;
+    CLOUDINARY: {
+        CLOUDINARY_CLOUD_NAME: string;
+        CLOUDINARY_API_KEY: string;
+        CLOUDINARY_API_SECRET: string;
+    }
 }
 
 
@@ -44,15 +49,18 @@ const loadEnvVariables = (): EnvConfig => {
         'REFRESH_TOKEN_EXPIRES_IN',
         'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
         'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE',
-         'EMAIL_SENDER_SMTP_USER',
+        'EMAIL_SENDER_SMTP_USER',
         'EMAIL_SENDER_SMTP_PASS',
         'EMAIL_SENDER_SMTP_HOST',
         'EMAIL_SENDER_SMTP_PORT',
         'EMAIL_SENDER_SMTP_FROM',
-         'GOOGLE_CLIENT_ID',
+        'GOOGLE_CLIENT_ID',
         'GOOGLE_CLIENT_SECRET',
         'GOOGLE_CALLBACK_URL',
-        'FRONTEND_URL'
+        'FRONTEND_URL',
+        'CLOUDINARY_CLOUD_NAME',
+        'CLOUDINARY_API_KEY',
+        'CLOUDINARY_API_SECRET',
     ]
 
     requireEnvVariable.forEach((variable) => {
@@ -74,17 +82,22 @@ const loadEnvVariables = (): EnvConfig => {
         REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
         BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
         BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string,
-         EMAIL_SENDER: {
+        EMAIL_SENDER: {
             SMTP_USER: process.env.EMAIL_SENDER_SMTP_USER as string,
             SMTP_PASS: process.env.EMAIL_SENDER_SMTP_PASS as string,
             SMTP_HOST: process.env.EMAIL_SENDER_SMTP_HOST as string,
             SMTP_PORT: process.env.EMAIL_SENDER_SMTP_PORT as string,
             SMTP_FROM: process.env.EMAIL_SENDER_SMTP_FROM as string,
         },
-       GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
         GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
         FRONTEND_URL: process.env.FRONTEND_URL as string,
+         CLOUDINARY: {
+            CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+            CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+            CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+        }
     }
 }
 
--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { SpecialtyController } from "./specialty.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
+import { multerUpload } from "../../config/multer.config";
 
 const router = Router();
 
-router.post("/", SpecialtyController.createSpecialty);
+router.post("/",
+    //  checkAuth(Role.ADMIN, Role.SUPER_ADMIN), 
+     multerUpload.single("file"), SpecialtyController.createSpecialty);
 router.get("/", SpecialtyController.getAllSpecialties);
 router.get("/:id", SpecialtyController.getSpecialty);
 router.delete('/:id', SpecialtyController.deleteSpecialty);
